91e9e7962e6d3d903b8707e406ea824863def8f4
ShivamMaru <101511109+ShivamMaru@users.noreply.github.com>
2024-06-01 21:05:54 +0530

    Time: 9 ms (66.12%), Space: 22.4 MB (54.00%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,20 +1,55 @@
 class Solution {
 public:
-    int trap(vector<int>& height) {
-        int ans = 0;
-        stack<int> st;
-        for(int i=0; i<height.size(); i++){
-            
-            while(!st.empty() && height[i] > height[st.top()]){
-                int base = st.top();
-                st.pop();
-                if(st.empty())
-                    break;
-                int dis = i - st.top()-1;
-                ans += (min(height[i], height[st.top()])- height[base]) * dis;
-            }
-            st.push(i);
+    int trap(vector<int>& h) {
+        
+
+        int mx = INT_MIN, ans = 0, l = 0, r = h.size() - 1, sum = 0;
+
+        for (int i = 0; i < h.size(); i++)
+            sum += h[i], mx = max(mx, h[i]);
+
+        for (int level = 1; level <= mx; level++) {
+            while (h[l] < level)
+                l++;
+            while (h[r] < level)
+                r--;
+            ans += r - l + 1;
         }
-        return ans;
+        return ans - sum;
     }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//         int ans = 0;
+//         stack<int> st;
+//         for(int i=0; i<height.size(); i++){
+            
+//             while(!st.empty() && height[i] > height[st.top()]){
+//                 int base = st.top();
+//                 st.pop();
+//                 if(st.empty())
+//                     break;
+//                 int dis = i - st.top()-1;
+//                 ans += (min(height[i], height[st.top()])- height[base]) * dis;
+//             }
+//             st.push(i);
+//         }
+//         return ans;
+    // }
 };
